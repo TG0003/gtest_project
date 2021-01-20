3ee35e3c21e1046caa844740e106b0354cdf1816
18061 <18061@windows10.microdone.cn>
2021-01-20 22:58:31 +0800

    gtest mod 4

--- src/main/java/com/myfirm/gtest/Test01.java
+++ src/main/java/com/myfirm/gtest/Test01.java
@@ -4,6 +4,6 @@ public class Test01 {
 	public void sayHello(String who) {
 		System.out.println("hello" + who);
 		System.out.println("gtest mod");
-		System.out.println("gtest mod3");
+		System.out.println("gtest mod4");
 	}
 }
