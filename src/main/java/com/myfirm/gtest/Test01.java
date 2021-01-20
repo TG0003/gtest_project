89e5f526692554af28ee6708b9581cab8a6595aa
18061 <18061@windows10.microdone.cn>
2021-01-20 22:41:59 +0800

    gtest2 mod 2th

--- src/main/java/com/myfirm/gtest/Test01.java
+++ src/main/java/com/myfirm/gtest/Test01.java
@@ -3,6 +3,5 @@ package com.myfirm.gtest;
 public class Test01 {
 	public void sayHello(String who) {
 		System.out.println("hello" + who);
-		System.out.println("gtest2 mod");
 	}
 }
--- src/main/java/com/myfirm/gtest/Test02.java
+++ src/main/java/com/myfirm/gtest/Test02.java
@@ -1,5 +1,7 @@
 package com.myfirm.gtest;
 
 public class Test02 {
-
+	public void a() {
+		System.out.println("gtest2 mod");
+	}
 }
